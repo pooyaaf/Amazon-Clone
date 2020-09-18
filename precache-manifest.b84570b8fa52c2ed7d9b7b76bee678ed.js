2fc50fa40d31cb16122af20696e6750fcd607211
pooyaaf <pooya1380@gmail.com>
2020-09-18 09:42:09 +0430

    Updates

---
+++ precache-manifest.b84570b8fa52c2ed7d9b7b76bee678ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f99e1f38d5e4b70a6b95bb710f44b9",
+    "url": "/github.io/Amazon-Clone/index.html"
+  },
+  {
+    "revision": "aab434938b716688e449",
+    "url": "/github.io/Amazon-Clone/static/css/main.24c0b97b.chunk.css"
+  },
+  {
+    "revision": "936897b2a36dfede9b48",
+    "url": "/github.io/Amazon-Clone/static/js/2.bc98f83a.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/github.io/Amazon-Clone/static/js/2.bc98f83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab434938b716688e449",
+    "url": "/github.io/Amazon-Clone/static/js/main.3f89a5fd.chunk.js"
+  },
+  {
+    "revision": "48fc5c101aa6ba4d5924",
+    "url": "/github.io/Amazon-Clone/static/js/runtime-main.171233a2.js"
+  }
+]);
